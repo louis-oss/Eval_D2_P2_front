b7587641e3233dea967e1a02497cc76a74a4bf47
Louis <louis.perrin@diiage.org>
2024-03-06 11:40:51 +0100

    first commit

---
+++ D2_P2_Louis_Perrin_front/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'D2_P2_Louis_Perrin_front';
+}
