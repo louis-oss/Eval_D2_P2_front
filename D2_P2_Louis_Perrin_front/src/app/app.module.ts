f1ab5ee8d40048241875e8d92453241ce42a1044
Louis <louis.perrin@diiage.org>
2024-03-07 09:31:06 +0100

    Add event creation form component

--- D2_P2_Louis_Perrin_front/src/app/app.module.ts
+++ D2_P2_Louis_Perrin_front/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { CreateEventComponent } from './create-event/create-event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventListComponent,
+    CreateEventComponent
   ],
   imports: [
     BrowserModule,
